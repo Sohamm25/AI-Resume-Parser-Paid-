37f07423047a4e45870fbe2ac733595139641bc7
Soham Pawar <154597186+Sohamm25@users.noreply.github.com>
2025-01-25 07:15:09 +0530

    Update resume parser.py

--- resume parser.py
+++ resume parser.py
@@ -6,7 +6,7 @@ def extract_text_from_pdf(pdf_path):
     try:
         with fitz.open(pdf_path) as pdf_document:
             for page_num in range(pdf_document.page_count):
-                page = pdf_document[page_num]
+                page = pdf_document[page_num]  
                 text += page.get_text("text")
         return text
     except Exception as e:
